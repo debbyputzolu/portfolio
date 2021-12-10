0c0ba73b8c99a42e794b531abd0aa8be0abda74e
debby-putzolu <debbymonchou@gmail.com>
2021-12-10 10:14:41 +0100

    front test

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <h1>helllllloooooo</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ducimus delectus porro optio perferendis ad velit nihil, eveniet, sint temporibus aliquam sequi debitis. Iure ullam dicta nihil aspernatur eum suscipit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime perspiciatis at, vitae labore recusandae non velit enim qui quibusdam quisquam distinctio consequuntur beatae repellendus. Nobis facilis voluptatibus, in, totam cumque fuga temporibus voluptates a mollitia non distinctio sit? Vero et error, laboriosam necessitatibus distinctio magnam eveniet ipsa qui sint ut!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+		-moz-animation: bg 60s linear infinite;
+		-webkit-animation: bg 60s linear infinite;
+		-ms-animation: bg 60s linear infinite;
+		animation: bg 60s linear infinite;
+		-moz-backface-visibility: hidden;
+		-webkit-backface-visibility: hidden;
+		-ms-backface-visibility: hidden;
+		backface-visibility: hidden;
+		-moz-transform: translate3d(0,0,0);
+		-webkit-transform: translate3d(0,0,0);
+		-ms-transform: translate3d(0,0,0);
+		transform: translate3d(0,0,0);
+	/* Set your background with this */
+
+		background: url("assets/bg.png") bottom left;
+		background-repeat: repeat-x;
+		height: 100%;
+		left: 0;
+		opacity: 1;
+		position: fixed;
+		top: 0;
+	}
 </style>
